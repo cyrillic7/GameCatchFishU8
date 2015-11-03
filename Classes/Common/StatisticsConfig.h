b0f83ccc6906d48721d23dfdfdf25adc467db979
冷星 <jieling_tan@163.com>
2015-11-03 10:46:40 +0800

    104

--- Classes/Common/StatisticsConfig.h
+++ Classes/Common/StatisticsConfig.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define k_url					"http://www.qicainiu.net/WS/WSClient.asmx/"				//url地址
-#define k_session_id			"103"															//渠道ID
+#define k_session_id			"104"															//渠道ID
 #define k_session_verion		"a01"														//渠道版本
